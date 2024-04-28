3330e54dd22b5ac240323a3bd4a564640ea62add
Sakshi Kumari <142109750+kumari-sakshi485@users.noreply.github.com>
2024-04-28 18:48:32 +0000

    done variable

--- 01_Basic/01_variable.js
+++ 01_Basic/01_variable.js
@@ -3,6 +3,8 @@ let accountWmail= 'sakshibhoj123@gmail.com'
 var accountPasword= '123456'
 
 accountCity= 'bihar'
+let accountState; /**undefine  output*/
+
 
 // ContactId = 2 // not allowed
 // accountWmail ='sakshicse@gmail.com' // allow
@@ -15,8 +17,10 @@ console.log( accountPasword);
 console.log(accountWmail);
 console.log(`user password is ${ accountPasword}`);
 console.log(accountCity);
-
-console.table([ContactId,accountPasword,accountWmail,accountCity])
+/* prefer not to use var
+because  of issue in block scope and functional scope
+*/
+console.table([ContactId,accountPasword,accountWmail,accountCity,accountState])
 // console.table krne se sare output  ek table form  mai  aajate hai like this
 // ─────────┬───────────────────────────┐
 // │ (index) │ Values                    │
@@ -26,3 +30,4 @@ console.table([ContactId,accountPasword,accountWmail,accountCity])
 // │ 2       │ 'sakshibhoj123@gmail.com' │
 // │ 3       │ 'bihar'                   │
 // └─────────┴───────────────────────────┘
+
