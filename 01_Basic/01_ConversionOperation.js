b222cbdeecc1c0f8c8e946d1f50e9df3e3bba35f
Sakshi Kumari <142109750+kumari-sakshi485@users.noreply.github.com>
2024-04-29 10:37:49 +0000

    conversion of data type in js

---
+++ 01_Basic/01_ConversionOperation.js
@@ -0,0 +1,28 @@
+// conversion:-
+
+
+
+// let score = "331234saddffff";
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//'33' => 33
+//"33fdfd" =>NaN
+//true =>1, false =>0
+
+let isLoggdin =1;
+let booleanIsLoggdin  = Boolean(isLoggdin)
+console.log(booleanIsLoggdin);
+
+// 1 =>true; 0 => false;
+// "" =>false
+// "Sakshi" =>true
+
+let someno = 33;
+let stringNumber = String(someno);
+console.log( typeof stringNumber); //33
+console.log(  stringNumber); //33
