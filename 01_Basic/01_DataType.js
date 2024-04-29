78c220c2d767ef85a47f65ed77e07c0acfb1e3de
Sakshi Kumari <142109750+kumari-sakshi485@users.noreply.github.com>
2024-04-29 10:24:39 +0000

    datatype

--- 01_Basic/01_DataType.js
+++ 01_Basic/01_DataType.js
@@ -1,3 +1,20 @@
 "use strict" ;  // traet all js code as newer version 
 // alert('hello')  //we are using node.js,not brower
+//primitive data types
+let name = 'sakshi' //strimg data type '',""
+let age= 18 //number data type
+let isLoggdin =  true //bollean data type it can be true and false or it can be 0 and 1
+
+//bigInt
+//null - stand alone value
+//undefine 
+// symbol - for find uniqueness
+
+
+//object
+
+console.log(typeof(name));
+console.log(typeof(isLoggdin));
+console.log(typeof null);
+console.log(typeof undefined);
 
