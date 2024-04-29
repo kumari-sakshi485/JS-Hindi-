459e29eb3fe33ed28d6edc6369387eb9cc932ae7
Sakshi Kumari <142109750+kumari-sakshi485@users.noreply.github.com>
2024-04-29 11:05:26 +0000

    comparison

---
+++ 01_Basic/04_Comparision.js
@@ -0,0 +1,17 @@
+console.log(2>1); //true
+console.log(2>=1); //true
+
+console.log(2==1); //false
+console.log(2===1); //false
+console.log(2!=1); //true
+
+// console.log("2" >1); //true
+// console.log("02" >1); //true
+
+// console.log(null > 0);//false   // avoide this type of comparision
+// console.log(null == 0); //false 
+// console.log(null>=0); //true
+
+//strict check it check value and data type 
+
+console.log("2" == 8); //false
