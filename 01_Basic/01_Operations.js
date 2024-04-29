a6f30f33f06e46d8a55c06a3bb211e1eef081f03
Sakshi Kumari <142109750+kumari-sakshi485@users.noreply.github.com>
2024-04-29 10:54:19 +0000

    operation

---
+++ 01_Basic/01_Operations.js
@@ -0,0 +1,35 @@
+// ********************Operations************************
+// basic
+let value = 3;
+let negValue = - value ;
+// console.log(negValue); //-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%6);
+// console.log(2/6);
+// console.log(2**5);
+
+let str1 = "hello" ;
+let str2 = "sakshi";
+let str3= str1 + str2 
+
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log((1+2)+"2");
+console.log("1" + 1+2);
+console.log((3+4) *5 %3);
+
+console.log(+true);
+console.log(+"");
+
+let gamecounter = 100
+gamecounter ++;
+// ++gamecounter ;
+
+console.log(gamecounter);
+
+//link for study
+// https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-numbers-and-dates
