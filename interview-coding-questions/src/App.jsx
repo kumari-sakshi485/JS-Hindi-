736aaa617be11fe251209d249ab323279ae67a44
Sakshi Kumari <142109750+kumari-sakshi485@users.noreply.github.com>
2024-05-01 17:45:44 +0000

    constructor

--- interview-coding-questions/src/App.jsx
+++ interview-coding-questions/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Coding1 from './Components/Coding1'
 import Coding2 from './Components/Coding2'
+import Coding3 from './Components/Coding3'
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
     <br />
     <h1>List Item</h1>
     <Coding2 />
+    <Coding3 />
     </>
   )
 }
