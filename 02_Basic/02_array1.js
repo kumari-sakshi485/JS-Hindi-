090dcff67c5860a8501af984715bf27b6301447a
Sakshi Kumari <142109750+kumari-sakshi485@users.noreply.github.com>
2024-05-09 17:08:00 +0000

    array

--- 02_Basic/02_array1.js
+++ 02_Basic/02_array1.js
@@ -18,3 +18,17 @@ console.log(allheros);
 const anotherArray = [1,2,3,[4,5,6],7,[6,7], [4,5]]
 const realArray = anotherArray.flat(Infinity)
 console.log(realArray);
+
+
+console.log(Array.isArray("sakshi")) //false 
+console.log(Array.from("sakshi"));  //[ 's', 'a', 'k', 's', 'h', 'i' ]
+
+console.log(Array.from({
+    name:"sakshi"
+}));  ///intresting (this can be give an empty array)
+
+let score1 = 100 
+let score2= 200
+let score3 =300
+
+console.log(Array.of(score1,score2,score3));
