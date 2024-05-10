5eb1278fb0b66ee13548ba7bc1b5d50c3a6738b8
Sakshi Kumari <142109750+kumari-sakshi485@users.noreply.github.com>
2024-05-10 12:23:36 +0000

    objects

---
+++ 02_Basic/04_OBJECTS.js
@@ -0,0 +1,65 @@
+// const tinderUser = new Object() //singleten  object
+
+const tinderUser  = {} //non-singleten object
+
+ tinderUser.id = 1
+ tinderUser.name = "sakshi"
+ tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+
+// nested object
+const regularUser = {
+    email : 'sakshi2@gmail.com',
+    fullname : {
+        username :{
+            fisrtname : 'sakshi',
+            secondname:"kumari"
+        }
+    }
+}
+
+console.log(regularUser.fullname.username.fisrtname);
+
+const obj1 = {
+    1:"a",
+    2:"b"
+}
+
+const obj2 = {
+    3:"c",
+    4:"c"
+}
+
+
+// const obj = {obj1,obj2}
+
+// const obj = Object.assign({},obj1,obj2)
+
+
+// most of the time we use spread operator for combine
+
+const obj = {...obj1, ...obj2}
+// console.log(obj);
+
+
+// syntax for ab database se value aayegi to value array of object aata hai
+// eg:
+
+const user = [
+    {
+        id : 1,
+        email : "sakshicse@gmail.com"
+    },
+    {
+        id:2,
+        email : "sakshigoogle.com"
+    },
+    {
+        id : 3,
+        email : "microsoft@gmail.com"
+    }
+]
+
+user[1].email
+console.log(user);
