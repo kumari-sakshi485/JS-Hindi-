d36de47c49180db57a73010c94b3a8aa702d5f3c
Sakshi Kumari <142109750+kumari-sakshi485@users.noreply.github.com>
2024-05-13 14:47:16 +0000

    function

--- 03_Basic/01_function.js
+++ 03_Basic/01_function.js
@@ -5,46 +5,102 @@ A JavaScript function is executed when "something" invokes it (calls it). */
 
 // eg
 
-function sayMyName(){
-    console.log("sakshi");
-}
+// function sayMyName(){
+//     console.log("sakshi");
+// }
 
 // sayMyName ///function ka refrence
 
 // sayMyName()  //execution
 
-function addTwoNumber(num1, num2){    //num1 and num2 is parameters
-    console.log(num1+num2);
+// function addTwoNumber(num1, num2){    //num1 and num2 is parameters
+//     // console.log(num1+num2);
 
-}
+// }
 
-addTwoNumber(3,7); // 3 and 7 is argument
+// addTwoNumber(3,7); // 3 and 7 is argument
 
 
 
-function addTwoNumber(num1, num2){    //num1 and num2 is parameters
+// function addTwoNumber(num1, num2){    //num1 and num2 is parameters
     // let result = num1+num2;
     // return result
 
 
-    return num1+num2
+    // return num1+num2
+
+// }
+
+// const result = addTwoNumber(5,10)
+// // console.log(`result is ${result}`);
+
+
+// function loginUserMessage(username = "SAKSHI"){
+//     if(!username){
+//         console.log(`please enter a username`);
+//         return 
+
+//     }
+
+// return `${username} just Logged in`
+// }
+
+// // console.log(loginUserMessage("sakshi"));
+// console.log(loginUserMessage("sam"));
+
+// function with obje t
+// eg2
+// ... ko spread operator bhi bolte h and rest bhi 
+
+function calculateCartPrice (val1,val2,...num1){ //... rest operator
+    return num1;
 
 }
 
-const result = addTwoNumber(5,10)
-// console.log(`result is ${result}`);
+console.log(calculateCartPrice(200,300,400,500,1000,"sakshi")); //[ 200, 300, 400,500,1000 ]
 
 
-function loginUserMessage(username = "SAKSHI"){
-    if(!username){
-        console.log(`please enter a username`);
-        return 
+// object ko kaise function mai use kiya jata hai and pass kiya jata hai hai uska eg niche hai
 
-    }
+// make object
 
-return `${username} just Logged in`
+const user= {
+    userName : "Sakshi",
+    price : 199
 }
 
-// console.log(loginUserMessage("sakshi"));
-console.log(loginUserMessage("sam"));
+function handleObject(anyObject){
+    console.log(`user name is ${anyObject.userName} and price is ${anyObject.price} `);
+}
+// handleObject(user)  //user name is Sakshi and price is 199 
+
+// ye to direct ho gya dusra tarika bhi hai object pass krne ka lets check
+
+// jab function ko call krenge tab usi mai object ko create kar deetehai 
+
+
+handleObject({
+    userName : "sakshi singh",
+    price : 456
+})
+
+// direct object pass kar sakte hai function jaruri nahi hai ki phle obj create kre
+
+// output :
+// user name is sakshi singh and price is 456 
+// user name is sakshi singh and price is 456 
+
+
+// yese hi array hi pass kar sakte hai function mai jaise object kiya 
+
+const myNewArray = [300,200,400,564,87779,7263,2536];
+function returnSecondValue(getArray){
+    return getArray[4]
+}
+
+// console.log(returnSecondValue(myNewArray)); //first method
+
+// second method
+console.log(returnSecondValue([400,600,578,9000,500]));
+
 
