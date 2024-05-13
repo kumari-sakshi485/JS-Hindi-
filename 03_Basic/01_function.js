6bf287246778dfcf143a7faf27ca7c996085af00
Sakshi Kumari <142109750+kumari-sakshi485@users.noreply.github.com>
2024-05-13 14:48:33 +0000

    function

--- 03_Basic/01_function.js
+++ 03_Basic/01_function.js
@@ -5,45 +5,45 @@ A JavaScript function is executed when "something" invokes it (calls it). */
 
 // eg
 
-// function sayMyName(){
-//     console.log("sakshi");
-// }
+function sayMyName(){
+    console.log("sakshi");
+}
 
-// sayMyName ///function ka refrence
+sayMyName ///function ka refrence
 
-// sayMyName()  //execution
+sayMyName()  //execution
 
-// function addTwoNumber(num1, num2){    //num1 and num2 is parameters
-//     // console.log(num1+num2);
+function addTwoNumber(num1, num2){    //num1 and num2 is parameters
+    console.log(num1+num2);
 
-// }
+}
 
-// addTwoNumber(3,7); // 3 and 7 is argument
-
-
-
-// function addTwoNumber(num1, num2){    //num1 and num2 is parameters
-    // let result = num1+num2;
-    // return result
+addTwoNumber(3,7); // 3 and 7 is argument
 
 
-    // return num1+num2
 
-// }
-
-// const result = addTwoNumber(5,10)
-// // console.log(`result is ${result}`);
+function addTwoNumber(num1, num2){    //num1 and num2 is parameters
+    let result = num1+num2;
+    return result
 
 
-// function loginUserMessage(username = "SAKSHI"){
-//     if(!username){
-//         console.log(`please enter a username`);
-//         return 
+    return num1+num2
 
-//     }
+}
 
-// return `${username} just Logged in`
-// }
+const result = addTwoNumber(5,10)
+// console.log(`result is ${result}`);
+
+
+function loginUserMessage(username = "SAKSHI"){
+    if(!username){
+        console.log(`please enter a username`);
+        return 
+
+    }
+
+return `${username} just Logged in`
+}
 
 // // console.log(loginUserMessage("sakshi"));
 // console.log(loginUserMessage("sam"));
